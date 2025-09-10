6ed06ab200d84c737ecdd7066bad968d41f294e8
milokv <milo.kvarnberg@gmail.com>
2025-09-10 23:20:35 +0200

    finished task, working with kattis sample inputs (yes im goated)

--- summera-tal/src/main.rs
+++ summera-tal/src/main.rs
@@ -31,12 +31,23 @@ use prompted::input;
 
 fn main() {
     let number_sequence_string: String = input!("Input list of positive integers (with spaces separating each one): ");
-    let n: u32 = input!("Please input the amount of positive integers in the list you just wrote: ").parse::<u32>().unwrap();
+    let mut n: u32 = input!("Please input the amount of positive integers in the list you just wrote: ").parse::<u32>().unwrap();
     let mut numbers_vector: Vec<u32> = parse_number_sequence(number_sequence_string);
+
     println!("{:?}", numbers_vector); // for testing
+
     numbers_vector.sort();
+
     println!("{:?}", numbers_vector); // for testing
-    println!("{}", n.to_string()) // for testing
+    println!("{}", n.to_string()); // for testing
+
+    n = divide_by_2(n);
+
+    println!("{}", n.to_string()); // for testing
+
+    let result: u32 = sum_biggest_half(numbers_vector, n);
+
+    println!("{}", result.to_string()); // for testing
 }
 
 
@@ -52,4 +63,13 @@ fn parse_number_sequence(numbers_string: String) -> Vec<u32> {
     .map(|x| x.parse::<u32>().unwrap())         // inspiration: https://stackoverflow.com/a/34090825 
     .collect();                                                                 //          ==||==
     return numbers_vector;
+}
+
+fn sum_biggest_half(v: Vec<u32>, n: u32) -> u32 {
+    let mut sum: u32 = 0;
+    let vector_length: u32 = v.len().try_into().unwrap();   
+    for i in (vector_length-n)..vector_length {
+        sum += v[i as usize];               //  "as usize" comes from this comment https://www.reddit.com/r/rust/comments/385em5/comment/crskat9/
+    }
+    return sum
 }
