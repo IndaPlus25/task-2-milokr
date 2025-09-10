3f22459f1be5cd609a18599f808ebf8bb736b7d5
milokv <milo.kvarnberg@gmail.com>
2025-09-10 21:17:29 +0200

    made rough sketch of code structure

---
+++ summera-tal/src/main.rs
@@ -0,0 +1,34 @@
+fn main() {
+    
+    /*notes:
+    input:
+    heltal n som anger tal i sekvens
+    n heltal separerade med space
+
+    output:
+    n/2 största tal ((n+1)/2 om udda)
+
+    (JÄTTE ROUGH) sketch:
+
+    int antalTal(n) {
+    if (n % 2) {    return n/2  }
+    else {  return (n+1)/2   }
+    }
+
+
+    let mut v = listaAvHeltal.parse()
+    v = v.sort()
+
+    let mut summa = 0
+
+    for i in range((v.len() - n), v.len()) {
+        summa = summa + v[i]
+    }
+
+    print(summa)
+    
+     */
+    
+
+
+}
