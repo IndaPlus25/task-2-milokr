9f4af8af38eb773456a31112e500b4e7435b7ad6
milokv <milo.kvarnberg@gmail.com>
2025-09-11 21:42:24 +0200

    working solution, doesn't seem to work on kattis though

--- cyber-clara/src/main.rs
+++ cyber-clara/src/main.rs
@@ -27,6 +27,8 @@
 
 use std::io::{self, Read};
 
+type Pair = (String, String);
+
 fn main() {
     let mut input = String::new();
     io::stdin()
@@ -46,8 +48,6 @@ fn main() {
         .map(|x| x.to_string())
         .collect();
 
-    type Pair = (String, String);
-
     let mut pair_vector: Vec<Pair> = Vec::new();
 
     for i in 0..amount_of_names {
@@ -56,5 +56,29 @@ fn main() {
         pair_vector.push((firstname, lastname));
     }
 
-    println!("{:?}", pair_vector);
+    let current_pair: usize = 0;
+    let names_to_remove: Vec<usize> = compare_pairs(current_pair, &pair_vector, amount_of_names);
+
+
+
+    for &i in names_to_remove.iter().rev() {        // got help from chatgpt with reversing this (to avoid issues with removing pairs from the vector affecting the index)
+        pair_vector.remove(i);
+    }
+    
+    println!("{:?}", pair_vector.len());
+}
+
+
+fn compare_pairs(current_pair: usize, list_of_pairs: &Vec<Pair>, vector_length: usize) -> Vec<usize> {
+    let mut names_to_remove: Vec<usize> = Vec::new();
+    for i in (current_pair+1)..vector_length {
+        if list_of_pairs[current_pair].0 == list_of_pairs[i].0 &&
+        list_of_pairs[current_pair].1 == list_of_pairs[i].1 {
+            names_to_remove.push(i);
+        }
+    }
+    if current_pair < vector_length {
+        names_to_remove.extend(compare_pairs(current_pair + 1, list_of_pairs, vector_length));
+    }
+    return names_to_remove;
 }
