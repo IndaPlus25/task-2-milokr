59aa006b20b0912cf8f02343fb08e120e0c8a225
milokv <milo.kvarnberg@gmail.com>
2025-09-11 18:26:39 +0200

    started sketching solution to cyber clara problem

--- cyber-clara/src/main.rs
+++ cyber-clara/src/main.rs
@@ -1,3 +1,28 @@
+/*  project sketch:
+
+    input: n
+        namn(0) efternamn(0) ... namn(n-1) efternamn(n-1)
+
+    let mut name_vector: Vec[String] = input
+        .split_whitespace()
+        .map(|x| x.parse::<String>().unwrap())
+        .collect(); 
+    name_vector = name_vector.remove(0);     // ta bort första entry, alltså siffran (n)
+
+    (is this even the right way of solving this? maybe create tuples and skip the vector step?)
+
+    let mut current_name_pair: u32 = 0;
+
+    type Pair = (String, String);
+
+    while (current_name_pair > name_vector.len();) == 0 {
+        let (ny pair) = (name_vector[current_name_pair as usize], name_vector[current_name_pair+1 as usize])        // problem: how do i make a new pair for every loop? maybe this isn't the right way to solve this either.
+        current_name_pair += 2
+    }
+
+*/
+
+
 fn main() {
-    println!("Hello, world!");
+    
 }
