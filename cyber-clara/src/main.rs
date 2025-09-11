2bd3df7b76e820981e0cd1e7cc7f8e9d7f2e6a5f
milokv <milo.kvarnberg@gmail.com>
2025-09-11 20:46:14 +0200

    working prototype to make vector with full names as pairs, no duplicate removal yet

--- cyber-clara/src/main.rs
+++ cyber-clara/src/main.rs
@@ -20,11 +20,41 @@
     for i in 0..amount_of_names {
         let firstname = input_vector[i].clone();
         let lastname = input_vector[i + amount_of_names].clone();           // chatgpt helped with formatting this part as i was more used to python lists and not at all used to ownership/borrowing (.clone())
-        pair_vector.push((firstname, lastname))
+        pair_vector.push((firstname, lastname));
     }
 
 */
 
+use std::io::{self, Read};
+
 fn main() {
-    
+    let mut input = String::new();
+    io::stdin()
+        .read_to_string(&mut input)         // chatgpt helped me make this take multiple lines of input
+        .unwrap();
+
+    let amount_of_names: usize = input
+        .split_whitespace()
+        .next()
+        .unwrap()
+        .parse()                            
+        .unwrap();
+
+    let input_vector: Vec<String> = input
+        .split_whitespace()
+        .skip(1)
+        .map(|x| x.to_string())
+        .collect();
+
+    type Pair = (String, String);
+
+    let mut pair_vector: Vec<Pair> = Vec::new();
+
+    for i in 0..amount_of_names {
+        let firstname = input_vector[i].clone();
+        let lastname = input_vector[i + amount_of_names].clone();           // chatgpt helped with formatting this part as i was more used to python lists and not at all used to ownership/borrowing (.clone())
+        pair_vector.push((firstname, lastname));
+    }
+
+    println!("{:?}", pair_vector);
 }
